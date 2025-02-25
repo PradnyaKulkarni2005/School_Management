63f3af00b31d0619d5734cfaed84e0b712044b9c
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-02-25 23:33:42 +0530

    Student Interface added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentInterface from './Components/StudentInterface';
+
+function App() {
+  return (
+    <div>
+      <StudentInterface/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/inter.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function inter() {
+  return (
+    <div>
+      <div style={{width: 1440, height: 1024, position: 'relative', background: 'white'}}>
+  <div style={{width: 604, height: 204, left: 65, top: 191, position: 'absolute', background: 'linear-gradient(180deg, #EDF2F4 0%, #DCE2E4 22%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 40}} />
+  <div style={{width: 1440, height: 151, left: 0, top: 0, position: 'absolute'}}>
+    <div style={{width: 1440, height: 151, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(90deg, #FDFFB6 0%, #E7BC91 100%)'}} />
+    <img style={{width: 99, height: 125, left: 72, top: 13, position: 'absolute'}} src="https://placehold.co/99x125" />
+    <div style={{width: 1020, height: 70, left: 258, top: 40, position: 'absolute', textAlign: 'center', color: '#540B0E', fontSize: 50, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word'}}>Eklavya Public School & Junior College</div>
+  </div>
+  <div style={{width: 94, height: 85, left: 90, top: 208, position: 'absolute', background: '#246A73', borderRadius: 9999}} />
+  <div style={{width: 321, height: 28, left: 112, top: 337, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Roll No:-</div>
+  <div style={{left: 214, top: 281, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Class :- </div>
+  <div style={{left: 213, top: 217, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Name:-</div>
+  <div style={{width: 1191, height: 267, left: 112, top: 497, position: 'absolute'}}>
+    <div style={{width: 222, height: 267, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #E3F2FD 0%, #9AD1D4 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 40}} />
+    <div style={{width: 222, height: 267, left: 323, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #E3F2FD 0%, #9AD1D4 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 40}} />
+    <div style={{width: 222, height: 267, left: 969, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #E3F2FD 0%, #9AD1D4 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 40}} />
+    <div style={{width: 222, height: 267, left: 646, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #E3F2FD 0%, #9AD1D4 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 40}} />
+    <div style={{width: 136, height: 30, left: 43, top: 9, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Personal Details</div>
+    <div style={{width: 155, height: 37, left: 361, top: 11, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Academics</div>
+    <div style={{width: 153, height: 24, left: 1003, top: 13, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Fees Status</div>
+    <div style={{width: 207, height: 24, left: 653, top: 13, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Progress & Improvement</div>
+    <img style={{width: 152, height: 141, left: 35, top: 103, position: 'absolute', borderRadius: 9999}} src="https://placehold.co/152x141" />
+    <img style={{width: 152, height: 141, left: 1006, top: 103, position: 'absolute', borderRadius: 9999}} src="https://placehold.co/152x141" />
+    <img style={{width: 152, height: 141, left: 681, top: 103, position: 'absolute', borderRadius: 9999}} src="https://placehold.co/152x141" />
+    <img style={{width: 152, height: 141, left: 356, top: 103, position: 'absolute', borderRadius: 9999}} src="https://placehold.co/152x141" />
+  </div>
+  <div style={{width: 1440, height: 129, left: 0, top: 896, position: 'absolute'}}>
+    <div style={{width: 1440, height: 129, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #DE796D 0%, #621708 100%)'}} />
+    <img style={{width: 88, height: 75, left: 433, top: 26, position: 'absolute', borderRadius: 35}} src="https://placehold.co/88x75" />
+    <img style={{width: 88, height: 75, left: 894, top: 26, position: 'absolute', borderRadius: 35}} src="https://placehold.co/88x75" />
+    <img style={{width: 88, height: 75, left: 731, top: 26, position: 'absolute', borderRadius: 35}} src="https://placehold.co/88x75" />
+    <img style={{width: 88, height: 75, left: 586, top: 26, position: 'absolute', borderRadius: 35}} src="https://placehold.co/88x75" />
+  </div>
+</div>
+    </div>
+  )
+}
